2a00a95dae4ca63d8b32f19955ecdcae33591c84
Fabrizio Albertetti <fabrizio.albertetti@he-arc.ch>
2023-03-15 16:07:19 +0100

    init import etudiants

---
+++ test_wheel.py
@@ -0,0 +1,6 @@
+from src.wallet import Wallet
+
+if __name__ == "__main__":
+    my_wallet = Wallet(100)
+    my_wallet.add_cash(120)
+    print("Congratulations. You successfully installed the TP2 package !")
