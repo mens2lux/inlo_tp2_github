5692f383f4734364fd6af3655e290d4c4f835a30
Fabrizio Albertetti <fabrizio.albertetti@he-arc.ch>
2023-03-21 12:58:38 +0100

    minor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="TP2",
-    version="0.1.0",
+    version="0.1.1",
     description="Travail pratique sur la qualité de code et le CI/CD",
     long_description=open("README.md").read(),
     author="prenom.nom",
